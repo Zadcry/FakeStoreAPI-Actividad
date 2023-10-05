644babc436bfa8ddf2361f47f2d030bf52901dee
Zafra <jzafra98@hotmail.com>
2023-10-05 00:25:18 -0500

    Selector de categorias funcional

--- src/ProductList.js
+++ src/ProductList.js
@@ -7,6 +7,7 @@ export function ProductList() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState("");
 
     useEffect(() => {
     fetch('https://fakestoreapi.com/products')
@@ -23,24 +24,33 @@ export function ProductList() {
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error.message}</div>;
+    
+    const handleCategoryChange = (event) => {
+        setSelectedCategory(event.target.value);
+    };
+
+    const filteredData = selectedCategory ? data.filter(product => product.category === selectedCategory) : data;
 
     return (
-    <>
-    <div className='ProductList'>
-        {data.map(product =>(
-            <>
-            <div className="Product">
-                <h2>
-                    <i>{product.title} </i>
-                </h2>
-                <img width="150" src={product.image} alt={product.category} />
-                <h2>${product.price}</h2>
-                <Link className='ProductNav' to={`products/${product.id}`}>Show More</Link>
+        <>
+        <select value={selectedCategory} onChange={handleCategoryChange}>
+                    <option value="">All Categories</option>
+                    {Array.from(new Set(data.map(product => product.category))).map(category => (
+                        <option key={category} value={category}>{category}</option>
+                    ))}
+                </select>
+            <div className='ProductList'>
+                
+                {filteredData.map(product => (
+                    <div className="Product" key={product.id}>
+                        <h2><i>{product.title}</i></h2>
+                        <img width="150" src={product.image} alt={product.category} />
+                        <h2>${product.price}</h2>
+                        <Link to={`products/${product.id}`}>Show More</Link>
+                    </div>
+                ))}
             </div>
-            </>
-        ))}
-    </div>
-    </>
+        </>
     );
 }
 
